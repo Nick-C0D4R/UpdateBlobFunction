a8869b69b1ac832c1dd53cdd1d4170019bf59e70
Mykola Kovshar <knikolay20031230@gmail.com>
2023-03-26 20:28:16 +0300

    Add email service

---
+++ UpdateBlob/Service/EmailService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Net.Mail;
+
+namespace FileUploadManager.Data
+{
+    public class EmailService
+    {
+        private SmtpClient _client;
+
+        public EmailService(SmtpClient client)
+        {
+            _client = client;
+            _client.UseDefaultCredentials = false;
+            _client.Credentials = new System.Net.NetworkCredential("File.upload.6.11@gmail.com", "pwhmrldpaqydqflu");
+            _client.EnableSsl = true;
+        }
+
+        public void Send(MailMessage letter)
+        {
+            _client.Send(letter);
+        }
+    }
+}
